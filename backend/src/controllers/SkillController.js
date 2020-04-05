1da08e69b503ed47515e22320c765eceeed3429a
Guilherme SG <silvagomesguilherme@hotmail.com>
2020-04-05 04:39:08 -0300

    Creating controllers for tests

---
+++ backend/src/controllers/SkillController.js
@@ -0,0 +1,18 @@
+class SkillController {
+    async store(req, res) {
+        const { name } = req.body
+        return res.json({ name })
+    }
+
+    async findByName(req, res) {
+        const { name } = request.params
+        return response.json({ name })
+    }
+
+    async findById(req, res) {
+        const { id } = request.params
+        return response.json({ id })
+    }
+}
+
+module.exports = new SkillController()
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,81 +1,16 @@
 const routes = require("express").Router()
 
-const gameSystem = require("./classes/GameSystem")
-const SkillSystem = require("./classes/skill/SkillSystem")
+const SkillController = require("./controllers/SkillController")
+const ActorController = require("./controllers/ActorController")
 
-const Actor = require("./classes/Actor")
-const Party = require("./classes/Party")
-const Skill = require("./classes/skill/Skill")
+routes.get("/skill/:name", SkillController.findByName)
+routes.get("/skill/:id", SkillController.findById)
 
-routes.get("/skill/:name", async (request, response) => {
-    const { name } = request.params
-    const skill = gameSystem.getSkillByName(name)
-    return response.json({
-        skill
-    })
-})
+routes.get("/skill-use", SkillController.use)
 
-routes.get("/skill/:id", async (request, response) => {
-    const { id } = request.params
-    const skill = gameSystem.getSkillById(id)
-    return response.json({
-        skill
-    })
-})
+routes.post("/skill", SkillController.store)
 
-routes.get("/skill-use", async (request, response) => {
-    const { casterId, subjectId, skillId } = request.body
-    
-    const skillSystem = new SkillSystem() 
-
-    gameSystem.setSelectedActor(subjectId)
-    skillSystem.useSkill(casterId, skillId)
-
-    const caster = gameSystem.getCaster()
-    const target = gameSystem.getSelectedActor()
-
-    return response.json({
-        caster: {
-            currentHP: caster.currentHP,
-            totalHP: caster.totalHP,
-            currentStamina: caster.currentStamina,
-            totalStamina: caster.totalStamina,
-            isAlive: caster.isAlive()
-        },
-        target: {
-            currentHP: target.currentHP,
-            totalHP: target.totalHP,
-            currentStamina: target.currentStamina,
-            totalStamina: target.totalStamina,
-            isAlive: target.isAlive()
-        },
-    })
-})
-
-routes.post("/skill", async (request, response) => {
-    const skill = new Skill(request.body)
-    gameSystem.registerSkill(skill)
-
-    return response.json({ id: skill.id })
-})
-
-routes.get("/actor/:name", async (request, response) => {
-    const { name } = request.params
-
-    const actor = gameSystem.getActorByName(name)
-
-    return response.json({
-        actor
-    })
-})
-
-routes.post("/actor", async (request, response) => {
-    const { name } = request.body
-
-    const actor = new Actor(name)
-    gameSystem.addActor(actor)
-
-    return response.json({ id: actor.id })
-})
+routes.get("/actor/:name", ActorController.findByName)
+routes.post("/actor", ActorController.store)
 
 module.exports = routes
