5010041309ab93a8a1ee5added31d735848158d8
Guilherme SG <silvagomesguilherme@hotmail.com>
2020-04-05 04:38:47 -0300

    Change babel to support arrow function inside classes

--- backend/__tests__/integration/skill.test.js
+++ backend/__tests__/integration/skill.test.js
@@ -1,15 +1,44 @@
-const mongoose = require("mongoose")
-const Skill = require("../../src/")
-
-mongoose.connect(process.env.DATABASE_TEST)
+const request = require("supertest")
+const app = require("../../src/app")
 
 describe("Skill API", () => {
-    it("Should receive skill name", () => {
-        const x = 2
-        const y = 10
+    const skillObj = {
+        "name": "Investida",
+        "description": "Avança em um inimigo, causando 25 (+1 cada 7 de Força) em um inimigo e atordoando-o logo depois. 8 rodadas de recarga.",
+        "cooldown": 8,
+        "paCost": 1,
+        "tags": {
+            "offensive": {
+                "damageType": "physic",
+                "subject": {
+                    "type": "actor"
+                },
+                "damageFunction": "byFormula",
+                "formula": "25 + Math.floor(caster.stats.for / 7)"
+            },
+            "disruptive": {
+                "statusList": [
+                    {
+                        "name": "stunned"
+                    }
+                ]
+            }
+        }
+    }
 
-        const sum = 12
+    it("Should receive skill", async () => {
+        const response = await request(app)
+            .post("/skill")
+            .send(skillObj)
 
-        expect(sum).toBe(12)
+        expect(response.status).toBe(200)    
+    })
+
+    it("Should return skill name", async () => {
+        const response = await request(app)
+            .post("/skill")
+            .send(skillObj)
+
+        expect(response.body.name).toBe(skillObj.name)    
     })
 })
